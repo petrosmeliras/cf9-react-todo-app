7f8fca688fd952649c6d0b680697369b10b04083
P.Meliras <petros.meliras@gmail.com>
2026-05-29 01:05:47 +0300

    feat(todo): add TodoList component

--- src/features/todo/types.ts
+++ src/features/todo/types.ts
@@ -8,3 +8,10 @@ export type TodoFormProps = {
   onAdd: (text: string) => void;
   inputRef: React.RefObject<HTMLInputElement | null >;
 }
+
+export type TodoListProps = TodoFormProps & {
+  todos: Todo[];
+  onToggle: (id: number) => void;
+  onEdit: (id: number, newText: string) => void;
+  onDelete: (id: number) => void;
+}
