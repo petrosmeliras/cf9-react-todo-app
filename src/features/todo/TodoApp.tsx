3d20b4a9a6a849459309f4d7dbd45bddf3db4a60
P.Meliras <petros.meliras@gmail.com>
2026-05-29 18:44:15 +0300

    feat(todo): add clear all button

--- src/features/todo/TodoApp.tsx
+++ src/features/todo/TodoApp.tsx
@@ -3,6 +3,7 @@ import {useRef} from "react";
 import useTodos from "./hooks/useTodos.ts";
 import TodoList from "./TodoList.tsx";
 import TodoStats from "./TodoStats.tsx";
+import Button from "../../shared/ui/Button.tsx";
 
 
 const TodoApp = () => {
@@ -30,6 +31,15 @@ const TodoApp = () => {
         />
 
         <TodoStats todos={todos}/>
+
+        {todos.length > 0 && (
+          <Button
+            addClasses="bg-cf-dark-red"
+            label="Clear All"
+            onClick={clearAll}
+          />
+        )}
+
       </div>
     </>
   )
